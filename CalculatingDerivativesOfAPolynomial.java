363530446be888f1d193732b8ca7852b95377aa6
Akassh Shah <akassh.shah77@gmail.com>
2017-02-25 23:36:39 -0500

    Adding javaclassfile

---
+++ CalculatingDerivativesOfAPolynomial.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class CalculatingDerivativesOfAPolynomial{
+   public getHighestDegreeofX{
+      System.out.println("Ener the highest of x in the whole equation");
+      Scanner scanInt=new Scanner();
+   }
+}
